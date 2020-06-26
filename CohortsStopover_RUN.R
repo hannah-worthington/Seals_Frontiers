3bb1ab1ad8187fa0b19ea81cacd8f7a6f2110eb8
hannah-worthington <hw233@st-andrews.ac.uk>
2020-06-26 17:04:09 +0100

    Added code to run models (one normal on arrivals)

--- CohortsStopover_RUN.R
+++ CohortsStopover_RUN.R
@@ -3,6 +3,7 @@ source('CohortsStopover_FUNCTIONS.R')
 
 # constants
 n.cohorts <- 4
+min.age <- 3
 
 # read in data
 data <- list()
@@ -19,6 +20,9 @@ for (c in 1:n.cohorts)  {
   K[c] <- length(data[[c]][1,])
 }
 
+# storage for AIC values
+AIC <- data.frame('arrivals model' = numeric(0), 'AIC' = numeric (0))
+
 
 
 ### One normal distribution for recruitment, constant p, constant phi
@@ -27,14 +31,36 @@ for (c in 1:n.cohorts)  {
 ### one normal distribution, mean and sd shared across cohorts
 param.1 <- c(0, 0, 0, 0)
 opt.1 <- nlm(likelihood_cohort, param.1, X = data, arr.dist = normal_one, arr.str = c('shared', 'shared'))
-res.1 <- normal_one(opt.1$estimate, c('shared', 'shared'), 3, 4, K)
+res.1 <- normal_one(opt.1$estimate, c('shared', 'shared'), min.age, n.cohorts, K)
+AIC <- rbind(AIC, data.frame('arrivals model' = 'one normal shared mean and sd', 'AIC' = 2*opt.1$minimum + 2*length(param.1))) 
 
 
+### model 2
+### one normal distribution, mean cohort dependent, sd shared across cohorts
+param.2 <- c(rep(0, n.cohorts), 0, 0, 0)
+opt.2 <- nlm(likelihood_cohort, param.2, X = data, arr.dist = normal_one, arr.str = c('cohort', 'shared'))
+res.2 <- normal_one(opt.2$estimate, c('cohort', 'shared'), min.age, n.cohorts, K)
+AIC <- rbind(AIC, data.frame('arrivals model' = 'one normal shared sd', 'AIC' = 2*opt.2$minimum + 2*length(param.2)))
 
+
+### model 3
+### one normal distribution, mean shared across cohorts, sd cohort dependent
+param.3 <- c(0, rep(0, n.cohorts), 0, 0)
+opt.3 <- nlm(likelihood_cohort, param.3, X = data, arr.dist = normal_one, arr.str = c('shared', 'cohort'))
+res.3 <- normal_one(opt.3$estimate, c('shared', 'cohort'), min.age, n.cohorts, K)
+AIC <- rbind(AIC, data.frame('arrivals model' = 'one normal shared mean', 'AIC' = 2*opt.3$minimum + 2*length(param.3)))
+
+
+### model 4
+### one normal distribution, mean and sd cohort dependent
+param.4 <- c(rep(0, n.cohorts), rep(0, n.cohorts), 0, 0)
+opt.4 <- nlm(likelihood_cohort, param.4, X = data, arr.dist = normal_one, arr.str = c('cohort', 'cohort'))
+res.4 <- normal_one(opt.4$estimate, c('cohort', 'cohort'), min.age, n.cohorts, K)
+AIC <- rbind(AIC, data.frame('arrivals model' = 'one normal cohort mean and sd', 'AIC' = 2*opt.4$minimum + 2*length(param.4)))
 
 
 param.test <- c(21.13785, 104.39981, -196.93070, 402.59331)
 test <- normal_one(param.1, c('shared', 'shared'), 3, 4, K)
-test2 <- onenormalbetas(exp(param.1[1]), exp(param.1[2]), K[1], 3)
+test2 <- onenormalbetas(exp(res.1$mu[1]), exp(res.1$sd[1]), K[1], 3)
 test3 <- HMM.str(test, n.cohorts, K)
 test4 <- likelihood_cohort(param.1, data, arr.dist = normal_one, arr.str = c('shared', 'shared'))
