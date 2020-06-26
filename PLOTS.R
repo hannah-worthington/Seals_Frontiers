4a3f4ab7c005734363ba36774fd4ece5a8c1ce2c
hannah-worthington <hw233@st-andrews.ac.uk>
2020-06-26 17:03:29 +0100

    Plotting arrival distributions
    Update to parameter names

--- PLOTS.R
+++ PLOTS.R
@@ -24,21 +24,21 @@ for (c in 1:n.cohorts)  {
 ### produce histograms of observed age at first recapture
 
 # find age at first breeding observation
-age_first <- list()
+age.first <- list()
 for (c in 1:n.cohorts)  {
   age <- rep(0, n[c])
   for (i in 1:n[c])  {
     age[i] <- which(data[[c]][i,] == 1)[1]
   }
-  age_first[[c]] <- age
+  age.first[[c]] <- age
 }
-age_first <- unlist(age_first)
+age.first <- unlist(age.first)
 
 # create data.frame for plotting
-age_plot <- data.frame('age' = age_first, 'cohort' = as.factor(rep(1991:1994, times = n)))
+age.plot <- data.frame('age' = age.first, 'cohort' = as.factor(rep(1991:1994, times = n)))
 
 # plot histograms of age of first recapture
-ggplot(data = age_plot) +
+ggplot(data = age.plot) +
   geom_histogram(mapping = aes(x = age, 
                                fill = cohort),
                  colour = 'black',
@@ -63,3 +63,39 @@ ggplot(data = age_plot) +
         axis.text = element_text(size = 12))
   
 
+
+
+### Produce plots from CohortsStopover_RUN model results
+
+# model number from CohortsStopover_RUN
+model <- 4
+
+# create data for plotting
+res <- eval(as.name(paste('res', model, sep='.')))
+res.plot <- data.frame('beta' = unlist(res$beta), 'cohort' = as.factor(rep(1991:1994, times = K)), 'age' = sequence(K))
+
+# plot arrival distributions with observed ages underneath
+ggplot() +
+  # add the observed ages
+  geom_histogram(data = age.plot,
+                 mapping = aes(x = age, 
+                               y = ..density..,
+                               fill = cohort),
+                 colour = 'black',
+                 binwidth = 1,
+                 alpha = 0.5) +
+  geom_line(data = res.plot, 
+            mapping = aes(x = age, 
+                          y = beta,
+                          col = cohort),
+            size = 2)  +
+  facet_wrap( ~ cohort)  +
+  scale_color_brewer(palette = 'Set1') +
+  scale_fill_brewer(palette = 'Set1') +
+  theme_minimal() +
+  theme(legend.position = 'none',
+        strip.text = element_text(face = 'bold',
+                                  size = 12),
+        axis.title = element_text(size = 12),
+        axis.text = element_text(size = 12))
+  
