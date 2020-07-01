646c1a42e137f3f4508da5a9178b1a6e30d321dd
hannah-worthington <hw233@st-andrews.ac.uk>
2020-07-01 17:18:52 +0100

    Changing save settings

--- PLOTS.R
+++ PLOTS.R
@@ -68,7 +68,7 @@ ggplot(data = age.plot) +
 ### Produce plots from CohortsStopover_RUN model results
 
 # model number from CohortsStopover_RUN
-model <- 4
+model <- 5
 
 # create data for plotting
 res <- eval(as.name(paste('res', model, sep='.')))
